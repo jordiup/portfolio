d5798cab71ab3f45b6ddc33cd8953f6a56323916
Jordi <jordi.hermoso.up@gmail.com>
2020-05-07 15:13:28 +0800

    2020-typescript

---
+++ 2020-typescript/gatsby-config.js
@@ -0,0 +1,50 @@
+require("ts-node").register({ files: true })
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: ``,
+    author: `@jordiup`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-chakra-ui",
+      options: {
+        /**
+         * @property {boolean} [isResettingCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        isResettingCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
+  ],
+}
